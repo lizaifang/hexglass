93559f5ad2032dcd4e8eda2e212d29794b16641a
a.michurin <a.michurin@22c52e74-88b3-cbee-dbf3-833f16ce0601>
2010-12-16 04:53:26 +0000

    Tag for stable release 1.2.0

---
+++ tags/hexglass-1.2.0/src/control/signal_gate.cpp
@@ -0,0 +1,40 @@
+/****
+    * HexGlass is a Tetris-like puzzle game.
+    *
+    * Project homepage: http://hexglass.googlecode.com/
+    *
+    * Copyright (C) 2010 Alexey Michurin <a.michurin@gmail.com>
+    *
+    * This program is free software: you can redistribute it and/or modify
+    * it under the terms of the GNU General Public License as published by
+    * the Free Software Foundation, either version 3 of the License, or
+    * (at your option) any later version.
+    *
+    * This program is distributed in the hope that it will be useful,
+    * but WITHOUT ANY WARRANTY; without even the implied warranty of
+    * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    * GNU General Public License for more details.
+    *
+    * You should have received a copy of the GNU General Public License
+    * along with this program. If not, see <http://www.gnu.org/licenses/>.
+*****/
+
+#include "signal_gate.h"
+
+SignalGate::SignalGate(QObject * p) :
+    QObject(p),
+    is_open(false)
+{
+}
+
+void
+SignalGate::open(bool o) {
+    is_open = o;
+}
+
+void
+SignalGate::in() {
+    if (is_open) {
+        emit out();
+    }
+}

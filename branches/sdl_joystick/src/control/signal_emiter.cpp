177cac3fbd7606e6c15f83c5c5989597971b857b
a.michurin <a.michurin@22c52e74-88b3-cbee-dbf3-833f16ce0601>
2010-12-17 14:06:40 +0000

    Create branch for joystick-driven version

---
+++ branches/sdl_joystick/src/control/signal_emiter.cpp
@@ -0,0 +1,32 @@
+/****
+    * HexGlass is a Tetris-like puzzle game.
+    *
+    * Project homepage: http://hexglass.googlecode.com/
+    *
+    * Copyright (C) 2010 Alexey Michurin <a.michurin@gmail.com>
+    *
+    * This program is free software: you can redistribute it and/or modify
+    * it under the terms of the GNU General Public License as published by
+    * the Free Software Foundation, either version 3 of the License, or
+    * (at your option) any later version.
+    *
+    * This program is distributed in the hope that it will be useful,
+    * but WITHOUT ANY WARRANTY; without even the implied warranty of
+    * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    * GNU General Public License for more details.
+    *
+    * You should have received a copy of the GNU General Public License
+    * along with this program. If not, see <http://www.gnu.org/licenses/>.
+*****/
+
+#include "signal_emiter.h"
+
+SignalEmiter::SignalEmiter(QObject * p) :
+    QObject(p)
+{
+}
+
+void
+SignalEmiter::operator()(int x) {
+    emit s(x);
+}
---
+++ branches/sdl_joystick/translations/hexglass_es.ts
@@ -0,0 +1,139 @@
+<!DOCTYPE TS><TS>
+<context>
+    <name>Dialogs</name>
+    <message>
+        <source>About</source>
+        <translation>Acerca de</translation>
+    </message>
+    <message>
+        <source>&lt;p&gt;Copyright (c) 2010 Alexey Michurin &amp;lt;a.michurin@gmail.com&amp;gt;&lt;/p&gt;&lt;p&gt;Released under the terms of the &lt;a href=&quot;http://www.gnu.org/licenses/&quot;&gt;GNU GPL v3&lt;/a&gt;&lt;/p&gt;&lt;p&gt;The </source>
+        <translation>&lt;p&gt;Copyright (c) 2010 Alexey Michurin &amp;lt;a.michurin@gmail.com&amp;gt;&lt;/p&gt;&lt;p&gt;Distribuido bajo los términos de la licencia &lt;a href=&quot;http://www.gnu.org/licenses/&quot;&gt;GNU GPL v3&lt;/a&gt;&lt;/p&gt;&lt;p&gt;</translation>
+    </message>
+    <message>
+        <source> is a Tetris-like puzzle game. 10 different types of blocks continuously fall from above and you must arrange them to make horizontal rows of hexagonal bricks. Completing any row causes those hexagonal blocks to disappear and the rest above move downwards. The blocks above gradually fall faster and the game is over when the screen fills up and blocks can no longer fall from the top.&lt;/p&gt;&lt;p&gt;Project home page: &lt;a href=&quot;http://hexglass.googlecode.com/&quot;&gt;http://hexglass.googlecode.com/&lt;/a&gt;&lt;/p&gt;</source>
+        <translation> es un juego de puzzle tipo Tetris. 10 distintos tipos de bloques caen continuamente desde la parte superior, por lo que deberá situarlas para formar filas horizontales de bloques hexagonales. Completando una fila de esos bloques hará que éstos desaparezcan y posteriormente los bloques superiores desciendan. Los bloques caerán cada vez más rápido y el juego terminará cuando la pantalla se llene de bloques y no puedan caer más desde arriba.&lt;/p&gt;&lt;p&gt;Página principal del Proyecto: &lt;a href=&quot;http://hexglass.googlecode.com/&quot;&gt;http://hexglass.googlecode.com/&lt;/a&gt;&lt;/p&gt;</translation>
+    </message>
+    <message>
+        <source>Help</source>
+        <translation>Ayuda</translation>
+    </message>
+    <message>
+        <source>&lt;h3&gt;Hot keys&lt;/h3&gt;&lt;dl&gt;&lt;dt&gt;Left/Right (arrows)&lt;/td&gt;&lt;dd&gt;Shift block&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;Up/Down (arrows)&lt;/td&gt;&lt;dd&gt;Rotate block&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;Space&lt;/td&gt;&lt;dd&gt;Drop block down or start new game&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;Enter&lt;/td&gt;&lt;dd&gt;Force one step to down&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;P&lt;/td&gt;&lt;dd&gt;Pause/unpause&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;N&lt;/td&gt;&lt;dd&gt;New game&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;1 ... 5&lt;/td&gt;&lt;dd&gt;Change size (and restart game)&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;A, S, D, F, G, H&lt;/td&gt;&lt;dd&gt;Change skin&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;Q&lt;/td&gt;&lt;dd&gt;Quit&lt;/dd&gt;&lt;/dl&gt;</source>
+        <translation>&lt;h3&gt;Controles&lt;/h3&gt;&lt;dl&gt;&lt;dt&gt;Izquierda/Derecha (flechas)&lt;/td&gt;&lt;dd&gt;Desplazar bloque&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;Arriba/Abajo (flechas)&lt;/td&gt;&lt;dd&gt;Rotar bloque&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;Espacio&lt;/td&gt;&lt;dd&gt;Descender bloque rápidamente o empezar nuevo juego&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;Enter&lt;/td&gt;&lt;dd&gt;Forzar un paso hacia abajo&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;P&lt;/td&gt;&lt;dd&gt;Pausar/Reanudar&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;N&lt;/td&gt;&lt;dd&gt;Nuevo juego&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;1 ... 5&lt;/td&gt;&lt;dd&gt;Cambiar el tamaño (y reiniciar el juego)&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;A, S, D, F, G, H&lt;/td&gt;&lt;dd&gt;Cambiar Apariencia&lt;/dd&gt;&lt;/dl&gt;&lt;dl&gt;&lt;dt&gt;Q&lt;/td&gt;&lt;dd&gt;Salir&lt;/dd&gt;&lt;/dl&gt;</translation>
+    </message>
+</context>
+<context>
+    <name>GlassWidget</name>
+    <message>
+        <source>pause</source>
+        <translation>pausa</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <source>Next</source>
+        <translation>Siguiente</translation>
+    </message>
+    <message>
+        <source>Score</source>
+        <translation>Puntuación</translation>
+    </message>
+    <message>
+        <source>Lines</source>
+        <translation>Líneas</translation>
+    </message>
+    <message>
+        <source>Level</source>
+        <translation>Nivel</translation>
+    </message>
+    <message>
+        <source>Game</source>
+        <translation>Juego</translation>
+    </message>
+    <message>
+        <source>New game</source>
+        <translation>Nuevo Juego</translation>
+    </message>
+    <message>
+        <source>Autopause mode</source>
+        <translation>Modo auto-pausa</translation>
+    </message>
+    <message>
+        <source>Size</source>
+        <translation>Tamaño</translation>
+    </message>
+    <message>
+        <source>Tiny</source>
+        <translation>Minúsculo</translation>
+    </message>
+    <message>
+        <source>Small</source>
+        <translation>Pequeño</translation>
+    </message>
+    <message>
+        <source>Normal</source>
+        <translation>Normal</translation>
+    </message>
+    <message>
+        <source>Big</source>
+        <translation>Grande</translation>
+    </message>
+    <message>
+        <source>Huge</source>
+        <translation>Enorme</translation>
+    </message>
+    <message>
+        <source>Skin</source>
+        <translation>Apariencia</translation>
+    </message>
+    <message>
+        <source>Mosaic normal</source>
+        <translation>Mosaico normal</translation>
+    </message>
+    <message>
+        <source>Mosaic big</source>
+        <translation>Mosaico grande</translation>
+    </message>
+    <message>
+        <source>Aqua normal</source>
+        <translation>Agua normal</translation>
+    </message>
+    <message>
+        <source>Aqua big</source>
+        <translation>Agua grande</translation>
+    </message>
+    <message>
+        <source>Help</source>
+        <translation>Ayuda</translation>
+    </message>
+    <message>
+        <source>About</source>
+        <translation>Acerca de</translation>
+    </message>
+    <message>
+        <source>About Qt</source>
+        <translation>Acerca de Qt</translation>
+    </message>
+    <message>
+        <source>Quit</source>
+        <translation>Salir</translation>
+    </message>
+    <message>
+        <source>Careful dropping</source>
+        <translation>Posicionamiento cuidadoso</translation>
+    </message>
+    <message>
+        <source>High scores</source>
+        <translation>Récord</translation>
+    </message>
+    <message>
+        <source>Mosaic tiny</source>
+        <translation>Mosaico pequeño</translation>
+    </message>
+    <message>
+        <source>Aqua huge</source>
+        <translation>Agua enorme</translation>
+    </message>
+</context>
+</TS>
